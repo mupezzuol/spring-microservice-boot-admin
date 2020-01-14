ddf049f2255197d1e7dfeaafa0a564afcc2e8837
Murillo Pezzuol <murillopezzuol@hotmail.com>
2020-01-14 11:19:46 -0300

    Upload Project

---
+++ src/main/java/br/com/microservice/boot/SpringMicroserviceBootAdminApplication.java
@@ -0,0 +1,13 @@
+package br.com.microservice.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroserviceBootAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroserviceBootAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/microservice/boot/SpringMicroserviceBootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.microservice.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroserviceBootAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
