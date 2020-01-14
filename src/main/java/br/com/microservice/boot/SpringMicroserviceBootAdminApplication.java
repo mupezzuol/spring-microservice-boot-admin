1f22fe41e7c530f25812bb78e29c8d06783c7edf
Murillo Pezzuol <murillopezzuol@hotmail.com>
2020-01-14 15:51:12 -0300

    Add Spring Boot Admin; Swagger; Actuator;

--- src/main/java/br/com/microservice/boot/SpringMicroserviceBootAdminApplication.java
+++ src/main/java/br/com/microservice/boot/SpringMicroserviceBootAdminApplication.java
@@ -1,9 +1,14 @@
 package br.com.microservice.boot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableAdminServer
 public class SpringMicroserviceBootAdminApplication {
 
 	public static void main(String[] args) {
